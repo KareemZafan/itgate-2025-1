cf5cf5485b36db319dde5b20e0f9a4a6916fd869
KareemZafan <kareemm.zafan@gmail.com>
2025-05-14 20:51:27 +0300

    Calling the created functions

--- src/calculator.py
+++ src/calculator.py
@@ -3,22 +3,19 @@
 def add(a,b):
  return a + b
  
-
-
-
 def sub(a,b):
  return a - b
  
-
-
 def mul(a,b):
  return a * b
  
- 
-
 def div(a,b):
 if b == 0: 
 	return None
  return a / b
  
+ print(add(2,4))
+ print(sub(2,4))
+ print(mul(2,4))
+ print(div(2,4))
  
