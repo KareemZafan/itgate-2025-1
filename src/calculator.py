ccadbe1a0991b0352d2125c758a715f70bb1c1fa
KareemZafan <kareemm.zafan@gmail.com>
2025-05-21 19:35:51 +0300

    Revert "This is change to canceled"
    This reverts commit 99d5dbf56100d204411ec01b1561e60fccf4833f.

--- src/calculator.py
+++ src/calculator.py
@@ -15,11 +15,6 @@ if b == 0:
  return a / b
  
  
-def is_even(a):
- return a % 2 == 0 
- 
- 
- 
 def get_square_root(a):
  	return math.sqrt(a)
  
@@ -28,5 +23,4 @@ print(sub(2,4))
 print(mul(2,4))
 print(div(2,4)) 
 print(get_square_root(100)) # 10
-print(is_even(100)) # True
  
