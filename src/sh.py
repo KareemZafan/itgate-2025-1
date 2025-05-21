01b52b1d7db6ec1508c423a115cda034398885f7
shetosbekham <shetosbekham@gmail.com>
2025-05-21 18:59:05 +0300

    shino modified 3 functhions

--- src/sh.py
+++ src/sh.py
@@ -2,4 +2,21 @@ print("Here is the file for Shenouda")
 
 print("^_^")
 
+
 # i removed the duplicated code 
+
+
+def add_numbers(a, b):
+    return a + b
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+def reverse_string(text):
+    return text[::-1]
+
+print(add_numbers(3, 5))      
+print(is_even(10))           
+print(reverse_string("hello")) 
