99d5dbf56100d204411ec01b1561e60fccf4833f
KareemZafan <kareemm.zafan@gmail.com>
2025-05-21 19:32:59 +0300

    This is change to canceled

--- src/calculator.py
+++ src/calculator.py
@@ -15,6 +15,11 @@ if b == 0:
  return a / b
  
  
+def is_even(a):
+ return a % 2 == 0 
+ 
+ 
+ 
 def get_square_root(a):
  	return math.sqrt(a)
  
@@ -23,4 +28,5 @@ print(sub(2,4))
 print(mul(2,4))
 print(div(2,4)) 
 print(get_square_root(100)) # 10
+print(is_even(100)) # True
  
--- src/sh.py
+++ src/sh.py
@@ -1,5 +1,2 @@
 print("Here is the file for Shenouda")
 
-print("^_^")
-
-# i removed the duplicated code 
