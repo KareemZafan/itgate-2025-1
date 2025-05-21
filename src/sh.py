2333f3797dc800c05d96c6906f610322357d6b87
KareemZafan <kareemm.zafan@gmail.com>
2025-05-21 18:58:17 +0300

    Adding Kareem's task

--- src/sh.py
+++ src/sh.py
@@ -2,4 +2,14 @@ print("Here is the file for Shenouda")
 
 print("^_^")
 
-# i removed the duplicated code 
+def print_name(name):
+    print(name)
+
+def abs(value):
+    if value > 0: 
+        return value
+    else return -1 * value
+    
+def print_list(a):
+for i in range(0, a.size()):
+    print(a[i])
